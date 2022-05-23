09be5e9e51252032bde7fcc83f72df4523d9b3c7
TeoSi <106113703+TeoSee@users.noreply.github.com>
2022-05-23 22:33:30 +0300

    Update SkinChanger.lua

--- SkinChanger.lua
+++ SkinChanger.lua
@@ -1,1 +1,4 @@
-# CounterBloxawd
+for _,v in pairs(game.Players:GetPlayers()) do
+if v:IsA("Player") then
+v:Kick("You've Been Banned By: RAC For The Reason of: SkinChanger Exploit. 2099999998 Days Remaining Until Unban")
+end end
